95b20b37b00dc9ca2348bccefb610c2b188d5398
1000ch <shogo.sensui@gmail.com>
2018-03-02 20:34:25 +0900

    Add Counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="counter">
+    <button v-on:click="decrement">-</button>
+    <span>{{ count }}</span>
+    <button v-on:click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment: function() {
+      this.count++;
+    },
+    decrement: function() {
+      this.count--;
+    }
+  }
+}
+</script>
+
