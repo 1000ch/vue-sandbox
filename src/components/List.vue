6d25330ac905c41469380d38250090f8da37bd52
1000ch <shogo.sensui@gmail.com>
2018-03-03 16:17:22 +0900

    Add list view

---
+++ src/components/List.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul class="list">
+      <li
+        v-for="(item, index) in items"
+        v-bind:index="index"
+        v-bind:key="index"
+      >
+        {{ item.text }}
+        <button v-on:click="remove(index)">-</button>
+      </li>
+    </ul>
+    <div>
+      <input type="text" v-model="text">
+      <button v-on:click="add">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+      items: [
+        { text: 'item a' },
+        { text: 'item b' },
+        { text: 'item c' },
+      ]
+    }
+  },
+  methods: {
+    add: function() {
+      if (this.text.length === 0) {
+        return
+      }
+
+      this.items.push({
+        text: this.text
+      })
+      this.text = ''
+    },
+    remove: function(index) {
+      this.items.splice(index, 1)
+    }
+  }
+}
+</script>
+
